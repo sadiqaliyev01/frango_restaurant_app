2c1300f8590a943871a24b825f4670a57d51d771
Ismayil Bashirov <beshirov.ismayil@gmail.com>
2024-10-31 12:01:06 +0400

    - ProductDetails page was created. The transition function to the product page was optimized and adapted for each product.

--- lib/presentation/screens/home_screen/widgets/lahmacun_card.dart
+++ lib/presentation/screens/home_screen/widgets/lahmacun_card.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:frango_restaurant_app/data/models//local/burger_data.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:frango_restaurant_app/utils/constants/app_colors.dart';
 import 'package:frango_restaurant_app/data/models/local/lahmacun_data.dart';
 import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/add_button.dart';
-import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_description.dart';
-import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_image.dart';
+import 'package:frango_restaurant_app/presentation/screens/product_details/product_details.dart';
 import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_name.dart';
 import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_price.dart';
-import 'package:frango_restaurant_app/utils/constants/app_colors.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_image.dart';
+import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_description.dart';
 
 class LahmacunCard extends StatelessWidget {
   const LahmacunCard({super.key});
@@ -15,15 +15,13 @@ class LahmacunCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<LahmacunData> lahmacunData = LahmacunData.getLahmacunData;
+
     return Card(
       color: AppColors.primaryYellow,
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24,
-              vertical: 8,
-            ),
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
             child: Align(
               alignment: Alignment.centerLeft,
               child: Text(
@@ -44,66 +42,82 @@ class LahmacunCard extends StatelessWidget {
               physics: const NeverScrollableScrollPhysics(),
               itemBuilder: (context, index) {
                 final data = lahmacunData[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12),
-                  child: Container(
-                    height: 120,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24),
-                      color: Colors.black,
-                    ),
-                    child: Row(
-                      children: [
-                         ProductImage(
+                return GestureDetector(
+                  onTap: () {
+                    // Ürün detay sayfasına geçiş
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProductDetails(
+                          productName: data.name,
                           imageLink:
-                          'assets/frango_images/image_${index + 31}.jpg',
-
-                         ),
-                        const SizedBox(width: 12),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const SizedBox(height: 12),
-                              ProductName(
-                                productName: data.name,
-                                style: GoogleFonts.roboto(
-                                  color: Colors.yellow,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12,
-                                ),
-                              ),
-                              const SizedBox(height: 12),
-                              ProductDescription(
-                                description: data.description,
-                                style: GoogleFonts.roboto(
-                                  color: Colors.yellow,
-                                  fontSize: 12,
-                                ),
-                              ),
-                              const SizedBox(height: 24),
-                              Expanded(
-                                child: Row(
-                                  children: [
-                                    ProductPrice(
-                                      price: data.price,
-                                      style: GoogleFonts.roboto(
-                                        color: Colors.yellow,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 12,
-                                      ),
-                                    ),
-                                    const SizedBox(width: 60),
-                                    const Expanded(
-                                      child: AddButton(),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
+                              'assets/frango_images/image_${index + 31}.jpg',
+                          description: data.description,
+                          price: data.price,
                         ),
-                      ],
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    child: Container(
+                      height: 120,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(24),
+                        color: Colors.black,
+                      ),
+                      child: Row(
+                        children: [
+                          ProductImage(
+                            imageLink:
+                                'assets/frango_images/image_${index + 31}.jpg',
+                          ),
+                          const SizedBox(width: 12),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const SizedBox(height: 12),
+                                ProductName(
+                                  productName: data.name,
+                                  style: GoogleFonts.roboto(
+                                    color: Colors.yellow,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const SizedBox(height: 12),
+                                ProductDescription(
+                                  description: data.description,
+                                  style: GoogleFonts.roboto(
+                                    color: Colors.yellow,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const SizedBox(height: 24),
+                                Expanded(
+                                  child: Row(
+                                    children: [
+                                      ProductPrice(
+                                        price: data.price,
+                                        style: GoogleFonts.roboto(
+                                          color: Colors.yellow,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      const SizedBox(width: 60),
+                                      const Expanded(
+                                        child: AddButton(),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
--- lib/presentation/screens/home_screen/widgets/qarnir_card.dart
+++ lib/presentation/screens/home_screen/widgets/qarnir_card.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:frango_restaurant_app/utils/constants/app_colors.dart';
 import 'package:frango_restaurant_app/data/models/local/qarnir_data.dart';
 import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/add_button.dart';
-import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_description.dart';
-import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_image.dart';
+import 'package:frango_restaurant_app/presentation/screens/product_details/product_details.dart';
 import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_name.dart';
+import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_image.dart';
 import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_price.dart';
-import 'package:frango_restaurant_app/utils/constants/app_colors.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:frango_restaurant_app/presentation/screens/home_screen/widgets/product_description.dart';
 
 class QarnirCard extends StatelessWidget {
   const QarnirCard({super.key});
@@ -43,65 +44,81 @@ class QarnirCard extends StatelessWidget {
               physics: const NeverScrollableScrollPhysics(),
               itemBuilder: (context, index) {
                 final data = qarnirData[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12),
-                  child: Container(
-                    height: 120,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24),
-                      color: Colors.black,
-                    ),
-                    child: Row(
-                      children: [
-                        ProductImage(
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProductDetails(
+                          productName: data.name,
                           imageLink:
                               'assets/frango_images/image_${index + 60}.jpg',
+                          description: data.description,
+                          price: data.price,
                         ),
-                        const SizedBox(width: 12),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const SizedBox(height: 12),
-                              ProductName(
-                                productName: data.name,
-                                style: GoogleFonts.roboto(
-                                  color: Colors.yellow,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12,
-                                ),
-                              ),
-                              const SizedBox(height: 12),
-                              ProductDescription(
-                                description: data.description,
-                                style: GoogleFonts.roboto(
-                                  color: Colors.yellow,
-                                  fontSize: 12,
-                                ),
-                              ),
-                              const SizedBox(height: 24),
-                              Expanded(
-                                child: Row(
-                                  children: [
-                                    ProductPrice(
-                                      price: data.price,
-                                      style: GoogleFonts.roboto(
-                                        color: Colors.yellow,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 12,
-                                      ),
-                                    ),
-                                    const SizedBox(width: 60),
-                                    const Expanded(
-                                      child: AddButton(),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    child: Container(
+                      height: 120,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(24),
+                        color: Colors.black,
+                      ),
+                      child: Row(
+                        children: [
+                          ProductImage(
+                            imageLink:
+                                'assets/frango_images/image_${index + 60}.jpg',
                           ),
-                        ),
-                      ],
+                          const SizedBox(width: 12),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const SizedBox(height: 12),
+                                ProductName(
+                                  productName: data.name,
+                                  style: GoogleFonts.roboto(
+                                    color: Colors.yellow,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const SizedBox(height: 12),
+                                ProductDescription(
+                                  description: data.description,
+                                  style: GoogleFonts.roboto(
+                                    color: Colors.yellow,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const SizedBox(height: 24),
+                                Expanded(
+                                  child: Row(
+                                    children: [
+                                      ProductPrice(
+                                        price: data.price,
+                                        style: GoogleFonts.roboto(
+                                          color: Colors.yellow,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      const SizedBox(width: 60),
+                                      const Expanded(
+                                        child: AddButton(),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
