858b81007371b4d21a852a3f282f83b1e202bd5a
Sadig Aliyev <aliyev.sdg@gmail.com>
2025-01-02 02:51:31 +0400

    - home auto synchronized scroll is done

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,10 @@
-import 'package:frango_restaurant_app/cubits/home/home_cubit.dart';
 import 'package:frango_restaurant_app/cubits/login/login_cubit.dart';
-import 'package:frango_restaurant_app/cubits/meal/meal_cubit.dart';
-import 'package:frango_restaurant_app/presentation/screens/home_screen/home_screen.dart';
 import 'package:frango_restaurant_app/presentation/screens/login_screen/login_screen.dart';
 import 'package:frango_restaurant_app/utils/di/locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frango_restaurant_app/presentation/screens/splash_screen/splash_screen.dart';
 import 'package:hive_flutter/adapters.dart';
-
-import 'cubits/category_names/category_names_cubit.dart';
-import 'data/remote/repositories/category_names_repository.dart';
-import 'data/remote/repositories/meal_repository.dart';
-import 'data/remote/services/remote/category_names_service.dart';
-import 'data/remote/services/remote/meal_service.dart';
 
 final navKey = GlobalKey<NavigatorState>();
 
@@ -36,24 +27,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         home:
-            isOnboardingCompleted ? MultiBlocProvider(
-             providers: [
-               BlocProvider(create: (context) => HomeCubit()),
-               BlocProvider(create: (context) =>  MealCubit(
-                 MealRepository(
-                   MealService(),
-                 ),
-               )..getMeals(),),
-               BlocProvider(
-                 create: (context) =>
-                 CategoryNamesCubit(
-                   CategoryNamesRepository(
-                     CategoryNamesService(),
-                   ),
-                 )..getCategoryNames(),
-               )
-             ],
-                child: const HomeScreen()) : const SplashScreen(),
+            isOnboardingCompleted ? const LoginScreen() : const SplashScreen(),
         debugShowCheckedModeBanner: false,
       ),
     );
