5456514abbb41d1d02813c07c19f76b8840bd41e
Ismayil Bashirov <beshirov.ismayil@gmail.com>
2025-03-01 01:39:03 +0400

    - Implement notifications feature with provider integration and dismiss functionality.

---
+++ lib/data/models/local/notification_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotificationModel {
+  final String title;
+  final String description;
+  final IconData icon;
+
+  NotificationModel({
+    required this.title,
+    required this.description,
+    required this.icon,
+  });
+}
